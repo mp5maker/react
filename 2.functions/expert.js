3d50395197915715c6e72722dc6efd7592d12342
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-09-06 23:25:28 +0600

    Added Composition

--- 2.functions/expert.js
+++ 2.functions/expert.js
@@ -50,3 +50,102 @@ const deepPick = (fields, object = {}) => {
 }
 console.log(deepPick('type', photon));
 console.log(deepPick('data.info', photon));
+
+/**
+ * Composition
+ */
+const template = "hh:mm:ss tt";
+const clockTime = template.replace("hh", "03")
+                        .replace("mm", "33")
+                        .replace("ss", "33")
+                        .replace("tt", "PM")
+console.log(clockTime);
+
+/**
+ * Goal of Composition: Genereate Higher Order Functions by combining simple functions
+ */
+
+// Simple Clock (Imperative Approach)
+function getClockTime(){
+    var date = new Date();
+    var time = "";
+
+    var time = {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+        AMPM: "AM"
+    }
+
+    if(time.hours == 12){
+        time.AMPM = "PM"
+    } else if (time.hours > 12) {
+        time.ampm = "PM"
+        time.hours -= 12;
+    }
+
+    if(time.hours < 10) {
+        time.hours = "0" + time.hours; 
+    }
+
+    if(time.minutes < 10) {
+        time.minutes = "0" + time.minutes;
+    }
+
+    if(time.seconds < 10) {
+        time.seconds = "0" + time.seconds;
+    }
+
+    return time.hours + ":" + time.minutes + ":" + time.seconds + " " + time.AMPM;
+}
+
+function logClockTime() {
+    var time = getClockTime();
+    console.clear();
+    console.log(time);
+}
+
+setInterval(logClockTime, 1000);
+
+/**
+ * Elegant Approach
+ */
+const serializeClockTime = date => 
+    ({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+    })
+
+const civilianHours = clockTime => 
+    ({
+        ...clockTime,
+        hours: (clockTime.hours > 12) ?
+            clockTime.hours - 12 :
+            clockTime.hours
+    })
+
+const appendAMPM = clockTime => 
+    ({
+        ...clockTime,
+        ampm: (clockTime.hours >= 12) ? "PM" : "AM"
+    })
+
+const prependZero = key => clockTime => 
+    ({
+        ...clockTime,
+        [key]: (clockTime[key] < 10) ? "0" + clockTime[key] : clockTime[key]
+    })
+
+const convertToCivilianTime = clockTime =>
+    compose(
+        appendAMPM,
+        civilianHours
+    )(clockTime)
+
+const doubleDigits = civilianTime =>
+    compose(
+        prependZero("hours"),
+        prependZero("minutes"),
+        prependZero("seconds")
+    )(civilianTime)
